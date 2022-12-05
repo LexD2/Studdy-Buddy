3c94f5abe300154d43f42c58796eb76bb4db20af
rcherry2 <114674168+rcherry2@users.noreply.github.com>
2022-12-05 13:05:50 -0500

    Add files via upload

---
+++ Ryans Study Buddy Folder/StudyBuddy/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for StudyBuddy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudyBuddy.settings')
+
+django_asgi_app = get_asgi_application()
+
+# need to make sure django asgi is initialized before this import
+import chat.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
